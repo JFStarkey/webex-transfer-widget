5f5527ddc04a661d02035f61781ded107bf61ed3
JFStarkey <jay_fluegge@starkey.com>
2026-09-04 08:47:52 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-alert("app.js loaded");
+const grid =
+  document.getElementById(
+    "buttonGrid"
+  );
+
+destinations.forEach(item => {
+
+  const button =
+    document.createElement(
+      "button"
+    );
+
+  button.className =
+    "transfer-btn";
+
+  button.textContent =
+    item.label;
+
+  button.onclick = () => {
+
+    alert(
+      item.dnis
+    );
+
+  };
+
+  grid.appendChild(button);
+
+});
+``
